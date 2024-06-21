ec844ac732a2ef77f77da5062c36973f38b01e02
jnvlnzl <102937800+jnvlnzl@users.noreply.github.com>
2024-06-20 23:28:12 -0400

    updated hide file
    + added a feature to allow the user to input a specific directory. if no directory is provided, the program will proceed with the current directory
    + added a file verification statement to check if the file exists

--- hide.sh
+++ hide.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 echo "Hide File"
-echo "Enter the filename in the current directory:"
+
+echo "Enter the directory path:" 
+read dir
+
+# Use current directory if user did not provide directory path
+dir = ${dir:-.}
+
+echo "Enter the filename in the specified directory"
 read file
-mv $file .$file
-echo "File successfully hidden."
+
+filepath = "$dir/$file"
+
+# Verify if the file exists
+if [-e "$filepath"]; then
+	mv "$filepath" "$dir/.$file"
+	echo "File successfully hidden."
+else
+	echo "File not found."
+fi
