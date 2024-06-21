b7a6e77314efc8cbc46d30d7b9fb304b57120d77
jnvlnzl <102937800+jnvlnzl@users.noreply.github.com>
2024-06-21 12:18:44 +0800

    list_hidden_files.sh [updated]

--- list_hidden_files.sh
+++ list_hidden_files.sh
@@ -4,13 +4,14 @@ echo "List Hidden Files"
 echo "Enter the directory path (Press Enter Key for current directory):"
 read dir
 
+# Use current directory if user did not provide directory path
 dir=${dir:-.}
 
 # Verify if the directory exists
 if [ -d "$dir" ]; then
 	hidden_files=$(ls -ld "$dir"/.* | grep -E '^d|^-')
 	if [ -n "$hidden_files" ]; then
-		echo "$hidden_files" | awk '{print $9}'
+		echo "$hidden_files" | awk '{print $9}' # Print 9th column in ls (filename only)
 	else
 		echo "No hidden files found in $dir."
 	fi
